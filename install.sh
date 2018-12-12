efbe9a2a40e92630a4487760d0cd27c706458715
windflex-sjlee <41511132+windflex-sjlee@users.noreply.github.com>
2018-12-12 15:03:30 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,9 +11,10 @@ yum install git -y
 git clone https://github.com/windflex-sjlee/linux_kernel_module.git
 
 # kernel module compile 
+cd linux_kernel_module
 make
 
 # insert kernel module
 insmod hello.ko
 insmod hello_2.ko
-
+cd ..
