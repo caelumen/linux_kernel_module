71707c827b5f75f2f988dca058a58cecc4bedabf
windflex-sjlee <41511132+windflex-sjlee@users.noreply.github.com>
2018-12-12 15:08:04 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-# kernel-header check or install
-ll /usr/src/kernels/
-yum install kernel-devel
-ls -l /usr/src/kernels/$(uname -r)
-yum install kernel-headers
-
-# git install
-yum install git -y
-git clone https://github.com/windflex-sjlee/linux_kernel_module.git
-
 # kernel module compile 
-cd linux_kernel_module
 make
 
 # insert kernel module
